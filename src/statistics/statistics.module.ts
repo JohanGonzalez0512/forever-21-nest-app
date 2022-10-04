74560807fad474ebd624513b0bc1f72d5241ebea
Johan Gonzalez <jopi20101@gmail.com>
2022-10-04 12:26:55 -0500

    Done (Statistics): Statistics Get first graph

--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { StatisticsService } from './statistics.service';
 import { StatisticsController } from './statistics.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movement, Product } from '../products/entities';
+import { Order, Orders_products } from '../orders/entities';
+import { User } from '../auth/entities/user.entity';
+
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Product, 
+      Movement,
+      Order, 
+      Orders_products,
+      User
+    ]),
+  ],
   controllers: [StatisticsController],
   providers: [StatisticsService]
 })
-export class StatisticsModule {}
+export class StatisticsModule { }
--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from '../products/entities/product.entity';
+import { Movement } from '../products/entities/movement.entity';
 
 @Injectable()
+
 export class StatisticsService {
 
-  findAll() {
-    return `This action returns all statistics`;
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Movement)
+    private readonly movementRepository: Repository<Movement>,
+  ) { }
+
+  async findAll() {
+    try {
+      const queryBuilder = this.movementRepository.createQueryBuilder();
+      const mostActiveUsers = await queryBuilder
+        .select('COUNT(*)', 'count')
+        .leftJoinAndSelect('Movement.user', 'user')
+        .groupBy('Movement.user.id, user.id')
+        .orderBy('count', 'DESC')
+        .getRawMany();
+      return mostActiveUsers;
+    } catch (error) {
+
+      console.log(error)
+    }
   }
 
   findOne(id: number) {
     return `This action returns a #${id} statistic`;
   }
 
-  
+
 }

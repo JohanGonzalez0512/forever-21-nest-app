2a2d988a7419357fea28e3c904525f3a7659838c
Johan Gonzalez <jopi20101@gmail.com>
2022-11-06 19:55:40 -0600

    bcrypt fixed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Office } from '../offices/entities/office.entity';
-import * as bcrypt from "bcrypt";
+import * as bcrypt from "bcryptjs";
 import { LoginUserDto, CreateUserDto } from './dto';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
 import { JwtService } from '@nestjs/jwt';
